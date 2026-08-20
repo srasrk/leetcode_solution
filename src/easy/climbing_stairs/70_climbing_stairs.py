70518a75a9dcc8ed2f55d1ed79c62f66bafd1989
Ali <sra.srk@gmail.com>
2026-08-20 19:11:26 +0530

    70 Climbing Stairs

---
+++ src/easy/climbing_stairs/70_climbing_stairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a = 1
+        b = 2
+
+        for i in range(3, n+1):
+            a, b = b, a + b  
+        return b
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    n = 3
+    result = solution.climbStairs(n)
+    print(f"Number of distinct ways to climb {n} stairs: {result}")
